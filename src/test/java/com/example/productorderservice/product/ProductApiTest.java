b3f787b36d63288aae194e4ae73699d11950c5df
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-03-08 21:29:27 +0900

    MAR.08.2023

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,10 +1,6 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -30,11 +26,7 @@ class ProductApiTest extends ApiTest {
 
         Long productId = 1L;
 
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/products/{productId}", productId)
-                .then().log().all()
-                .extract();
+        final var response = ProductSteps.상품수정요청(productId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
---
+++ src/test/java/com/example/productorderservice/product/ProductTest.java
@@ -0,0 +1,21 @@
+package com.example.productorderservice.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void update() {
+        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
+
+        product.update("상품수정", 2000, DiscountPolicy.NONE);
+
+        assertThat(product.getName()).isEqualTo("상품수정");
+        assertThat(product.getPrice()).isEqualTo(2000);
+        assertThat(product.getDiscountPolicy()).isEqualTo(DiscountPolicy.NONE);
+    }
+}
