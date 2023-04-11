d7c598c378fd721d079bf4a6312cda5321c0da87
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-04-11 15:05:06 +0900

    APR.11.2023

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,14 +1,29 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 import static org.springframework.util.Assert.isTrue;
 import static org.springframework.util.Assert.notNull;
 
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Order {
-    private final Product product;
-    private final int quantity;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
+
 
     Order(final Product product, final int quantity) {
         notNull(product, "상품은 필수입니다.");
@@ -17,11 +32,4 @@ public class Order {
         this.quantity = quantity;
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
