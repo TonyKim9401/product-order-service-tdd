909e012a424a20d389b4ecfeaef5706c39e3a31f
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-04-11 15:05:15 +0900

    APR.11.2023

--- src/main/java/com/example/productorderservice/order/OrderRepository.java
+++ src/main/java/com/example/productorderservice/order/OrderRepository.java
@@ -1,19 +1,19 @@
 package com.example.productorderservice.order;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
-class OrderRepository {
+interface OrderRepository extends JpaRepository<Order, Long> {
 
 
-    private Map<Long, Order> persistence = new HashMap<>();
+    /*private Map<Long, Order> persistence = new HashMap<>();
     private Long sequence = 0L;
 
     public void save(final Order order) {
         order.assignId(++sequence); // 주문시 id 값을 1씩 증가시킴
         persistence.put(order.getId(), order);
-    }
+    }*/
+
 }
