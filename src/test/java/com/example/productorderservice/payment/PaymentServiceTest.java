f2518aca50838b0d32caebd5b055a73a3db08105
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-04-16 15:14:13 +0900

    APR.16.2023

--- src/test/java/com/example/productorderservice/payment/PaymentServiceTest.java
+++ src/test/java/com/example/productorderservice/payment/PaymentServiceTest.java
@@ -1,12 +1,28 @@
 package com.example.productorderservice.payment;
 
+import com.example.productorderservice.order.OrderService;
+import com.example.productorderservice.order.OrderSteps;
+import com.example.productorderservice.product.ProductService;
+import com.example.productorderservice.product.ProductSteps;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class PaymentServiceTest {
 
+    @Autowired
     private PaymentService paymentService;
-    private PaymentPort paymentPort;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private ProductService productService;
+
+
+    /*private PaymentPort paymentPort;
 
     @BeforeEach
     void setUp() {
@@ -14,12 +30,18 @@ class PaymentServiceTest {
         final PaymentRepository paymentRepository = new PaymentRepository();
         paymentPort = new PaymentAdapter(paymentGateway, paymentRepository);
         paymentService = new PaymentService(paymentPort);
-    }
+    }*/
 
     @Test
     void 상품주문() {
-        final PaymentRequest request = PaymentSteps.주문결제요청_생성();
+        //상품 등록
+        productService.addProduct(ProductSteps.상품등록요청_생성());
 
+        //상품 주문
+        orderService.createOrder(OrderSteps.상품주문요청_생성());
+
+        //상품 주문 결제
+        final PaymentRequest request = PaymentSteps.주문결제요청_생성();
         paymentService.payment(request);
     }
 
